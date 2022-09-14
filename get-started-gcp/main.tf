b16a860d1eff3e2e59750df39499620accd292fb
bkshewale99 <bkshewale5@gmail.com>
2022-09-14 16:07:13 +0530

    add storage bucket

---
+++ get-started-gcp/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.36.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "qwiklabs-gcp-02-1e9a1b848d88"
+  region = "us-central1"
+  zone = "us-central1-a"
+}
+
+resource "google_storage_bucket" "GCS" {
+  name = "bks-tf"
+}
